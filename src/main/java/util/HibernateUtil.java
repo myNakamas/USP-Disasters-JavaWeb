d1a0101a68015c654f6b3c1b4bbd9201d116e7b3
Martin <luffymugiwarano115@gmail.com>
2021-03-19 19:42:28 +0200

    hibernate cfg file

---
+++ src/main/java/util/HibernateUtil.java
@@ -0,0 +1,28 @@
+package util;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.Query;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.metadata.ClassMetadata;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().
+                    configure("util/hibernate.cfg.xml").
+                    buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return sessionFactory.openSession();
+    }
+
+
+}
