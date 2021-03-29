8aedca956de82ec9cade52bdbd516c70686c47c8
Angel <angel.g.prodanov@gmail.com>
2021-03-29 14:03:38 +0300

    HibernateUtil

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -1,28 +1,64 @@
 package util;
 
-import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
-import org.hibernate.Session;
-import org.hibernate.Query;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.metadata.ClassMetadata;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
 
 public class HibernateUtil {
-    private static final SessionFactory sessionFactory;
 
-    static {
-        try {
-            sessionFactory = new Configuration().
-                    configure("util/hibernate.cfg.xml").
-                    buildSessionFactory();
-        } catch (Throwable ex) {
-            throw new ExceptionInInitializerError(ex);
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+
+        if (sessionFactory == null) {
+
+            try {
+
+                Configuration configuration = new Configuration();
+
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+
+                Properties settings = new Properties();
+
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/db_usp_disasters?useSSL=false&createDatabaseIfNotExist=true&serverTimezone=UTC&useLegacyDatetimeCode=false");
+
+                settings.put(Environment.USER, "root");
+
+                settings.put(Environment.PASS, "");
+
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL57InnoDBDialect");
+
+                settings.put(Environment.SHOW_SQL, "true");
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(settings);
+
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+
         }
-    }
 
-    public static Session getSession() throws HibernateException {
-        return sessionFactory.openSession();
-    }
+        return sessionFactory;
 
+    }
 
 }
