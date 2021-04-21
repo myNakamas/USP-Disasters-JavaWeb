841216f6429241e4231a982e9ee6a03f5271de9d
Angel <Angel.G.Prodanov@gmail.com>
2021-04-21 14:14:18 +0300

    small fixes

--- src/main/webapp/js/themeFunctions.js
+++ src/main/webapp/js/themeFunctions.js
@@ -69,17 +69,20 @@ function getCookie(name) {
 
     function onSignIn(googleUser) {
     // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    //var profile = googleUser.getBasicProfile();
+/*    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
     console.log('Full Name: ' + profile.getName());
     console.log('Given Name: ' + profile.getGivenName());
     console.log('Family Name: ' + profile.getFamilyName());
     console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
+    console.log("Email: " + profile.getEmail());*/
 
     //so, we can get the data from here, now... how do we send with a post request?
 
     // The ID token you need to pass to your backend:
     var id_token = googleUser.getAuthResponse().id_token;
     console.log("ID Token: " + id_token);
+    document.getElementById("google-token").value = id_token;
+    document.getElementById('loginForm').submit();
+
 }
